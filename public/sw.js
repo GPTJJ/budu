cd75934f86ce7378f9c5ee1e687678a1419c9502
GPTJJ <yuegu1995@gmail.com>
2026-08-07 18:31:37 +0800

    feat: add capybara PWA app icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,12 @@
-const CACHE_NAME = 'budu-shell-v1'
-const APP_SHELL = ['/', '/manifest.webmanifest', '/favicon.svg']
+const CACHE_NAME = 'budu-shell-v2'
+const APP_SHELL = [
+  '/',
+  '/manifest.webmanifest',
+  '/favicon-capybara.png',
+  '/apple-touch-icon.png',
+  '/app-icon-capybara-192.png',
+  '/app-icon-capybara-512.png',
+]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
